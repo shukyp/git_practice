d1690a562149c7ccd941cad86bac156e43b24c57
Shuky Persky <shukyp@bezeqint.net>
2022-01-21 19:23:59 +0200

    appl_d.c added

---
+++ src/appl/appl_d.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+/*
+    Operatorts
+    --------------
+    Arithmatic: +, -, *, /, %       [compound:   +=, -=, *=, /=, %=]
+    Relational: <, <=, >, >=, ==, !=
+    Logical:    &&, ||, !
+    Bitwise:    &, |, ~(negate), ^(xor), <<(shift), >>(shift)  [compound: &=, |=, <<=, >>=] 
+    Incremenet|Decremenet: ++, --
+    Assignment: =
+
+*/
+
+void appl_ddd(void)
+{
+	// do nothing
+}
+
